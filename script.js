ac20bae20474a555ee116001850e588a6da47093
Darko <Darko@DESKTOP-Q7UO6H4>
2022-12-17 09:02:45 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+
+let bookmarks = [];
+
+// Show Modal, Focus on Input
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () =>
+  modal.classList.remove("show-modal")
+);
+window.addEventListener("click", (event) =>
+  event.target === modal ? modal.classList.remove("show-modal") : false
+);
+
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression =
+    /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert("Please provide a valid web address!");
+    return false;
+  }
+
+  // Valid
+  return true;
+}
+
+// Build Bookmarks DOM
+function buildBookmarks() {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = "";
+
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark("${url}")`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    // Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    // Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "Add Bookmark",
+        url: "https://youtube.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, index) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(index, 1);
+    }
+  });
+
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+// Handle Data from Form
+function storeBookmark(event) {
+  event.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+
+  if (!urlValue.includes("http://") && !urlValue.includes("https://")) {
+    urlValue = `https://${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
+
+//JSON metoda stringify pretvara JS objekat ili vrednost u JSON, dok JSON metoda parse radi obrnuto
